ad4e37a4dd3a0bca31e3e4d3ba7c8c6bdf1b48b4
Kevin Griffin <kgriffin@meditech.com>
2021-04-26 13:21:56 -0400

    fixed issue with utils.numToUSD function

--- Utilities.js
+++ Utilities.js
@@ -9,7 +9,7 @@
 
 var utils = (function () {
   "use strict";
-  
+
   function dollarsToNum(dollars) {
     if (typeof dollars === "number") {
       return dollars;
@@ -26,19 +26,19 @@ var utils = (function () {
     if (dollars.match(/^\$\d+(,\d{3})*(\.\d+)?\sMillion$/)) {
       return Number(
         dollars.match(/\d+(,\d{3})*(\.\d+)?/)[0].replace(/,/g, "")
-        ) * 1000000;
+      ) * 1000000;
     }
     if (dollars.match(/^\$\d+(,\d{3})*(\.\d+)?\sBillion$/)) {
       return Number(
         dollars.match(/\d+(,\d{3})*(\.\d+)?/)[0].replace(/,/g, "")
-        ) * 1000000000;
+      ) * 1000000000;
     }
     return undefined;
   }
-  
+
   /**
-  * @returns {object} today's with hours,minutes,seconds,and ms set to 0.
-  */
+   * @returns {object} today's with hours,minutes,seconds,and ms set to 0.
+   */
   function getSimpleDate() {
     var today = new Date();
     today.setHours(0);
@@ -47,23 +47,35 @@ var utils = (function () {
     today.setMilliseconds(0);
     return today;
   }
-  
+
   function numToUSD(numStr) {
-    return "$" + Number(numStr).toLocaleString(
-      "en-US",
-        {
-          "style":"currency",
-          "currency":"USD"
-        }
+    const units = {
+      B: "Billion",
+      K: "Thousand",
+      M: "Million"
+    };
+    const matchResult = Number(numStr).toLocaleString(
+      "en-US", {
+        currency: "USD",
+        style: "currency",
+        notation: "compact",
+        minimumSignificantDigits: 2
+      }
+    ).match(/^(\$\d+(\.\d+)?)([BKM])?$/);
+    const formattedResult = (
+      (matchResult[3]) ?
+      matchResult[1] + " " + units[matchResult[3]] :
+      matchResult[1]
     );
+    return formattedResult;
   }
-  
+
   return {
     dollarsToNum: dollarsToNum,
     getSimpleDate: getSimpleDate,
     numToUSD: numToUSD
   };
-  
+
 }());
 
 //******************************************************************************
@@ -71,9 +83,9 @@ var utils = (function () {
 function fixDatesUtil() {
   "use strict";
   var kittyBalanceSheet = SpreadsheetApp.getActive()
-  .getSheetByName("Balance Sheet");
+    .getSheetByName("Balance Sheet");
   var kittyBalanceArr = kittyBalanceSheet.getDataRange()
-  .getValues();
+    .getValues();
   kittyBalanceArr.forEach(
     function (kittyRow, index) {
       var row = index + 1;
@@ -85,10 +97,10 @@ function fixDatesUtil() {
       if (typeof kittyRow[0] === "number") {
         dateNumStr = kittyRow[0].toString();
         date = new Date(dateNumStr.slice(0, 4),
-                        dateNumStr.slice(4, 6),
-                        dateNumStr.slice(6, 8));
+          dateNumStr.slice(4, 6),
+          dateNumStr.slice(6, 8));
         kittyBalanceSheet.getRange(row, column, numRows, numColumns)
-        .setValue(date);
+          .setValue(date);
       }
     });
 }
